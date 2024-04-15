f08d32d6d34e5a02b9b83565cd8b56580a7c7c2a
Bude8 <henryblee8@gmail.com>
2024-04-15 20:59:09 +0100

    Chapter 5.1 - halfway through before trying Spectre replace

---
+++ cmd/web/templates.go
@@ -0,0 +1,9 @@
+package main
+
+import "github.com/Bude8/lets-go/internal/models"
+
+// html/template only allows one item of dynamic data when rendering a template
+// This allows multiple pieces of dynamic data
+type templateData struct {
+	Snippet models.Snippet
+}
