07a21ca16b08b2d4aa51b820e303bf3ef1946b3a
Bude8 <henryblee8@gmail.com>
2024-04-15 21:43:55 +0100

    Chapter 5.3 - template cache

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -1,10 +1,43 @@
 package main
 
-import "github.com/Bude8/lets-go/internal/models"
+import (
+	"html/template"
+	"path/filepath"
+
+	"github.com/Bude8/lets-go/internal/models"
+)
 
 // html/template only allows one item of dynamic data when rendering a template
 // This allows multiple pieces of dynamic data
 type templateData struct {
 	Snippet  models.Snippet
 	Snippets []models.Snippet
+}
+
+func newTemplateCache() (map[string]*template.Template, error) {
+	// Initialise a new map to act as the cache
+	cache := map[string]*template.Template{}
+
+	pages, err := filepath.Glob("./ui/html/pages/*.tmpl.html")
+	if err != nil {
+		return nil, err
+	}
+
+	for _, page := range pages {
+		name := filepath.Base(page)
+		files := []string{
+			"./ui/html/base.tmpl.html",
+			"./ui/html/partials/nav.tmpl.html",
+			page,
+		}
+
+		ts, err := template.ParseFiles(files...)
+		if err != nil {
+			return nil, err
+		}
+
+		cache[name] = ts
+	}
+
+	return cache, nil
 }
