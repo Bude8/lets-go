89d6a63105fb56ddcc7bac32440650c48d98d028
Bude8 <henryblee8@gmail.com>
2024-04-21 11:40:18 +0100

    Chapter 8 - done

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,10 +7,13 @@ import (
 	"log/slog"
 	"net/http"
 	"os"
+	"time"
 
 	// No code used, but we need the init() function to run, so that the driver can register
 	// itself with the database/sql package
 	"github.com/Bude8/lets-go/internal/models"
+	"github.com/alexedwards/scs/mysqlstore"
+	"github.com/alexedwards/scs/v2"
 	"github.com/go-playground/form/v4"
 	_ "github.com/go-sql-driver/mysql"
 )
@@ -21,10 +24,11 @@ type config struct {
 }
 
 type application struct {
-	logger        *slog.Logger
-	snippets      *models.SnippetModel
-	templateCache map[string]*template.Template
-	formDecoder   *form.Decoder
+	logger         *slog.Logger
+	snippets       *models.SnippetModel
+	templateCache  map[string]*template.Template
+	formDecoder    *form.Decoder
+	sessionManager *scs.SessionManager
 }
 
 func main() {
@@ -56,11 +60,16 @@ func main() {
 
 	formDecoder := form.NewDecoder()
 
+	sessionManager := scs.New()
+	sessionManager.Store = mysqlstore.New(db)
+	sessionManager.Lifetime = 12 * time.Hour
+
 	app := &application{
-		logger:        logger,
-		snippets:      &models.SnippetModel{DB: db},
-		templateCache: templateCache,
-		formDecoder:   formDecoder,
+		logger:         logger,
+		snippets:       &models.SnippetModel{DB: db},
+		templateCache:  templateCache,
+		formDecoder:    formDecoder,
+		sessionManager: sessionManager,
 	}
 
 	logger.Info("Starting server on", slog.Any("addr", cfg.addr))
--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -13,16 +13,18 @@ func (app *application) routes() http.Handler {
 	// Note that the path given to the http.Dir function is relative to the project
 	// directory root.
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
-
 	// Use the mux.Handle() function to register the file server as the handler for
 	// all URL paths that start with "static/". For matching paths, we strip the
 	// "/static" prefix before the request reaches the file server.
 	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
 
-	mux.HandleFunc("GET /{$}", app.home)
-	mux.HandleFunc("GET /snippet/view/{id}", app.getSnippetView)
-	mux.HandleFunc("GET /snippet/create", app.getSnippetCreate)
-	mux.HandleFunc("POST /snippet/create", app.postSnippetCreate)
+	// New middleware chain specific to our dynamic app routes.
+	dynamic := alice.New(app.sessionManager.LoadAndSave)
+
+	mux.Handle("GET /{$}", dynamic.ThenFunc(app.home))
+	mux.Handle("GET /snippet/view/{id}", dynamic.ThenFunc(app.getSnippetView))
+	mux.Handle("GET /snippet/create", dynamic.ThenFunc(app.getSnippetCreate))
+	mux.Handle("POST /snippet/create", dynamic.ThenFunc(app.postSnippetCreate))
 
 	// Used for every request our application receives
 	standard := alice.New(app.recoverPanic, app.logRequest, commonHeaders)
